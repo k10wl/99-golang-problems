ce914e5d1ec80e689fdcc94ae2bb40b67e89e274
k10wl <aleks.antonsheko@gmail.com>
2023-11-18 21:45:27 +0200

    init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module 99-golang-problems
+
+go 1.21.2
---
+++ utils.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+type Case struct {
+	name     string
+	arg      any
+	expected any
+}
+
+func (c *Case) errorInfo(actual any) string {
+	return fmt.Sprintf("\nError: %s\nexpected: %v\nactual: %v", c.name, c.expected, actual)
+}
